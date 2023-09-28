8996cbbb6d8ea41e893b76492254467c0026b589
Gayanuka Bulegoda <grbulegoda@gmail.com>
2023-09-28 18:19:59 +0530

    HumanPlayer backEnd

--- src/main/java/lk/ijse/dep/service/BoardImpl.java
+++ src/main/java/lk/ijse/dep/service/BoardImpl.java
@@ -6,6 +6,8 @@ public class BoardImpl implements Board {
     private Piece[][] pieces = new Piece[6][5];
     private BoardUI boardUI;
     public BoardImpl(BoardController boardController) {
+        //Object created using BoardUI interface (BoardController) assigned to its Parent class Reference BoardUI
+        boardUI = boardController;
         //emptying the pieces of the board
         for (int i=0;i < 6;i++) {
             for (int j=0;j < 5;j++){
@@ -14,9 +16,9 @@ public class BoardImpl implements Board {
         }
     }
 
-    public BoardImpl(BoardUI boardUI) {
-        this.boardUI = boardUI;
-    }
+//    public BoardImpl(BoardUI boardUI) {
+//        this.boardUI = boardUI;
+//    }
 
     @Override
     public BoardUI getBoardUI() {
--- src/main/java/lk/ijse/dep/service/HumanPlayer.java
+++ src/main/java/lk/ijse/dep/service/HumanPlayer.java
@@ -3,6 +3,21 @@ package lk.ijse.dep.service;
 public class HumanPlayer extends Player {
     public HumanPlayer(Board newBoard) {
         super();
+    }
 
+    @Override
+    public void movePiece(int col) {
+        if (board.isLegalMove(col)) {
+            board.updateMove(col, Piece.BLUE);
+            board.getBoardUI().update(col, true);
+
+            if (board.findWinner().getWinningPiece() == Piece.EMPTY) {
+
+                if (!board.exitLegalMoves()) {
+                    board.getBoardUI().notifyWinner(board.findWinner());
+                }
+            }
+            else board.getBoardUI().notifyWinner(board.findWinner());
+        }
     }
 }
