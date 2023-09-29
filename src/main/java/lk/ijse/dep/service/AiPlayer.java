91b4032ff459610b7105008777076ebbf2cba8f4
Gayanuka Bulegoda <grbulegoda@gmail.com>
2023-09-29 11:26:49 +0530

    AiPlayer backEnd Update1

--- src/main/java/lk/ijse/dep/service/AiPlayer.java
+++ src/main/java/lk/ijse/dep/service/AiPlayer.java
@@ -12,7 +12,7 @@ public class AiPlayer extends Player {
             // *6 bcz Math.random() generates random decimal no.s between 0.0 and 1.0
             colRandom =  (int) (Math.random() * 6);
 
-        } while (!(colRandom > -1 && colRandom < 6));
+        } while (!(colRandom > -1 && colRandom < 6) || !(board.isLegalMove(colRandom)));
 
         if (board.isLegalMove(colRandom)) {
             board.updateMove(colRandom, Piece.GREEN);
